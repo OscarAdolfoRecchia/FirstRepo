c80a05758a988607abfba0a2f003f4dd9d35c70e
Oscar Recchia <oscaradolforecchia@oscars-mbp.italy.itroot.adnet>
2017-03-08 12:40:57 +0100

    Prima versione con 2 Log nell Delegate e nel bottone

--- MyGitApp/ViewController.m
+++ MyGitApp/ViewController.m
@@ -18,6 +18,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)bottonePremuto:(UIButton *)sender {
+    
+    NSLog(@"bottone premuto");
+}
 
 
 - (void)didReceiveMemoryWarning {
