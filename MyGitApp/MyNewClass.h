3caae90a8de0b9b035fee7dc025448fd243801fe
Oscar Recchia <oscaradolforecchia@oscars-mbp.italy.itroot.adnet>
2017-03-08 14:46:39 +0100

    Aggiunta di una classe

---
+++ MyGitApp/MyNewClass.h
@@ -0,0 +1,13 @@
+//
+//  MyNewClass.h
+//  MyGitApp
+//
+//  Created by Oscar Adolfo Recchia on 3/8/17.
+//  Copyright © 2017 Oscar Adolfo Recchia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyNewClass : NSObject
+
+@end
---
+++ MyGitApp/MyNewClass.m
@@ -0,0 +1,13 @@
+//
+//  MyNewClass.m
+//  MyGitApp
+//
+//  Created by Oscar Adolfo Recchia on 3/8/17.
+//  Copyright © 2017 Oscar Adolfo Recchia. All rights reserved.
+//
+
+#import "MyNewClass.h"
+
+@implementation MyNewClass
+
+@end
